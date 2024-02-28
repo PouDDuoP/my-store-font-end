a6ce1753fdd759e591150117e471630d2432a50e
PouDDuoP <kevinalvarado.ag@gmail.com>
2024-02-28 12:22:23 -0400

    feat Mostrando los componentes
    Aplicada dinámica sencilla para entender como se relacionan dos componentes mediante la importación de los mismos.

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ProductComponent } from './../../components/product/product.component'
+
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ProductComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
