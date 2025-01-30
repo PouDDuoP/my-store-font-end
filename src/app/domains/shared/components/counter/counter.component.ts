a215b1b3c5c0ba9c57605be7b9fd0cb8cc5267ff
PouDDuoP <kevinalvarado.ag@gmail.com>
2025-01-30 10:50:58 -0400

    feat Ciclo de vida de componentes: ngOnInit
    creados ngOnInit, ngAfterViewInit, ngOnDestroy en counter para ver el ciclo de vida de cada uno

--- src/app/domains/shared/components/counter/counter.component.ts
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -15,15 +15,40 @@ export class CounterComponent {
   constructor() {
     // NO ASYNC
     // Before render
+    // Corre solo una vez
     console.log('constructor');
     console.log('-'.repeat(10));
   }
 
   ngOnChanges(changes: SimpleChange) {
     // Befor and during render
+    // Puede correr multiples veces
     console.log('ngOnChanges');
     console.log('-'.repeat(10));
     console.log(changes);
+  }
+
+  ngOnInit() {
+    // after render
+    // Corre solo una vez
+    // IS ASYNC, iedal for (async, then, subs)
+    console.log('ngOnInit')
+    console.log('-'.repeat(10));
+    console.log('duration =>', this.duration);
+    console.log('message =>', this.message);
+  }
+
+  ngAfterViewInit() {
+    // after render
+    // hijos ya fueron render?
+    console.log('ngAfterViewInit')
+    console.log('-'.repeat(10));
+  }
+
+  ngOnDestroy() {
+    // before destroy
+    console.log('ngOnDestroy')
+    console.log('-'.repeat(10));
   }
 
 }
