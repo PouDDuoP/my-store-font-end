9a78f9ab93f9f38d8cd75c55e03915cf91d111b9
PouDDuoP <kevinalvarado.ag@gmail.com>
2025-01-31 17:29:09 -0400

    faet Creando el Header
    Agregado el componente header y configurado en list

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { ProductComponent } from './../../components/product/product.component'
 import { Product } from './../../../shared/models/product.model'
+import { HeaderComponent } from "../../../shared/components/header/header.component";
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule, ProductComponent],
+  imports: [CommonModule, ProductComponent, HeaderComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
@@ -16,6 +17,30 @@ export class ListComponent {
 
   constructor() {
     const initProducts: Product[] = [
+      {
+        id: Date.now(),
+        title: 'Product 1',
+        description: 'This is a product',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=23',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Product 2',
+        description: 'This is a product',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=24',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Product 2',
+        description: 'This is a product',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=25',
+        creationAt: new Date().toISOString()
+      },
       {
         id: Date.now(),
         title: 'Product 1',
